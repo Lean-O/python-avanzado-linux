b87932e0224dfc05ba073bf15c909c942710ccfc
Leandro Orozco <orozcolean@gmail.com>
2026-09-15 03:16:27 +0000

    modificaciones

--- app/app/charts.py
+++ app/app/charts.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(name, labels, values):
-  fig, ax = plt.subplots()
-  ax.bar(labels, values)
-  plt.show()
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.savefig('pie.png')
+    plt.close()
 
 def generate_pie_chart(labels, values):
-  fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
-  ax.axis('equal')
-  plt.show()
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    plt.savefig('bar.png')
+    plt.close()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
